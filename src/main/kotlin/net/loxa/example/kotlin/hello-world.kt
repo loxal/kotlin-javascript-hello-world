3ea08e3a533e5eb8355aeb0e555030bf9298ef34
Alex <alexander.orlov@loxal.net>
2015-01-15 18:07:01 +0100

    add run configuration for local web server; completely adjust to Maven structure

--- src/main/kotlin/net/loxa/example/kotlin/hello-world.kt
+++ src/main/kotlin/net/loxa/example/kotlin/hello-world.kt
@@ -6,13 +6,19 @@ import org.w3c.dom.Element
 public class HelloWorld {
 }
 
-fun main(args: Array<String>) {
+val container: Element = document.getElementById("container")
+
+fun main(vararg args: String) {
     println("JavaScript generated through Kotlin")
 
     sayHelloViaDom()
+    sayHelloViaInlinedJavaSript()
 }
 
 private fun sayHelloViaDom() {
-    val container: Element = document.getElementById("container")
-    container.textContent = "Hello, DOM!"
+    container.textContent = "Hello, DOM! Kotlin is writing..."
+}
+
+private fun sayHelloViaInlinedJavaSript() {
+    js("document.writeln('Hello, from inlined JavaScript in Kotlin!')")
 }
